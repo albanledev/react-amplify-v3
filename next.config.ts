6bffded8cd3d5e7969101d97b0344a1d711ebe55
Alban de Braquilanges <alban.debraquilanges@canal-plus.com>
2025-07-23 14:46:47 +0200

    monitoring metrics

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+      async rewrites() {
+        return [
+            {
+                source: '/metrics',
+                destination: '/api/metrics',
+            },
+        ];
+    },
 };
 
+
+
 export default nextConfig;
+
